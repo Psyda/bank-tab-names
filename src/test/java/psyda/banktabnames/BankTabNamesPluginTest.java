a80b3bd339b1b48f08a30ee3ac9be70a499937c3
Saita <travisneon@hotmail.com>
2020-06-21 16:25:00 -0400

    Added BankTabNames Plugin

--- src/test/java/psyda/banktabnames/BankTabNamesPluginTest.java
+++ src/test/java/psyda/banktabnames/BankTabNamesPluginTest.java
@@ -1,13 +1,14 @@
-package com.example;
+package psyda.banktabnames;
 
+import psyda.banktabnames.BankTabNamesPlugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class BankTabNamesPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(BankTabNamesPlugin.class);
 		RuneLite.main(args);
 	}
 }
